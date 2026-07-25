015a2db82bd7203b984c58bee05f0a1ead639076
John Salata <jrsalata@sep.com>
2026-07-25 15:26:25 -0400

    Created Search Builder to return URLs

---
+++ enums/comparison.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Comparison(Enum):
+    EQUAL = "="
+    NOT_EQUAL = "!="
+    GREATER_THAN = ">"
+    GREATER_THAN_OR_EQUAL = ">="
+    LESS_THAN = "<"
+    LESS_THAN_OR_EQUAL = "<="
